515878af9d5cc86c80ac1df1b91c274f36b06a46
kor1999 <kor1999@mail.ru>
2018-10-30 15:38:51 +0300

    Succesfully added Improved Euler method

--- src/ImpEulerMethod.java
+++ src/ImpEulerMethod.java
@@ -1,2 +1,18 @@
+import org.jfree.data.xy.XYSeries;
+
 public class ImpEulerMethod {
+    private XYSeries impEulerSeries;
+
+    public XYSeries getImpEulerSeries() {
+        return impEulerSeries;
+    }
+
+    public ImpEulerMethod(double x0, double y0, double xf, double h){
+        impEulerSeries = new XYSeries("Improved Euler method");
+        impEulerSeries.add(x0,y0);
+        for (double j = x0 + h; j <xf ; j=j+h) {
+            impEulerSeries.add(j,j + h * Math.pow(Math.E,-Math.sin(j+h/2))
+                    - (j+(h/2)*Math.pow(Math.E,-Math.sin(j)) - j*Math.cos(j))*Math.cos(j+h/2));
+        }
+    }
 }
--- src/MakingGraph.java
+++ src/MakingGraph.java
@@ -12,7 +12,9 @@ public class MakingGraph {
 
         XYSeriesCollection xySerColl = new XYSeriesCollection();
         EulerMethod eulerMethod = new EulerMethod(x0,y0,xf,h);
+        ImpEulerMethod impEulerMethod = new ImpEulerMethod(x0,y0,xf,h);
         xySerColl.addSeries(eulerMethod.getEulerSeries());
+        xySerColl.addSeries(impEulerMethod.getImpEulerSeries());
         JFreeChart chart = ChartFactory.createXYLineChart("y=e^(-sin(x))-y*cos(x)",
                 "x","y",xySerColl,PlotOrientation.VERTICAL,true,true,true);
 
